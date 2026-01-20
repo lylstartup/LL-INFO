173da83431dfe76e6d721c8991f9b11ec903b055
lylstartup <lyl.startup@gmail.com>
2026-01-20 19:51:20 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/LL-info/",
   server: {
     host: "::",
     port: 8080,
